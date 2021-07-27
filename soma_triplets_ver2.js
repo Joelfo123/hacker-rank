800d8b02dcdbbc2ea7493323ced015d383f0decf
jguerreiro <joel.borghi@gmail.com>
2021-07-27 17:20:01 -0300

    commit

---
+++ soma_triplets_ver2.js
@@ -0,0 +1,92 @@
+
+function comparaTriplets( a, b ) {
+    
+    var resultado = [ 0, 0 ];
+
+    var scoreA = 0;
+    var scoreB = 0;
+
+    // index [ 0 ]
+     
+    if ( a[ 0 ] > b[ 0 ] ) {
+
+        ++scoreA;
+        resultado[ 0 ] = scoreA;
+
+    } 
+
+    else if ( a[ 0 ] < b[ 0 ] ) {
+
+        ++scoreB;
+        resultado[ 1 ] = scoreB;
+
+
+    } else {
+
+        a[ 0 ] = 0;
+        b[ 0 ] = 0;
+        
+    }
+
+    // index [ 1 ]
+    
+    if ( a[ 1 ] > b[ 1 ] ) {
+
+        ++scoreA;
+        resultado[ 0 ] = scoreA;
+        
+    } 
+
+    else if ( a[ 1 ] < b[ 1 ] ) {
+
+        ++scoreB;
+        resultado[ 1 ] = scoreB;
+
+
+    } else {
+
+        a[ 1 ] = 0;
+        b[ 1 ] = 0;
+        
+    }
+
+    // index [ 2 ]
+
+    if ( a[ 2 ] > b[ 2 ] ) {
+
+        ++scoreA;
+        resultado[ 0 ] = scoreA;
+        
+    } 
+
+    else if ( a[ 2 ] < b[ 2 ] ) {
+
+        ++scoreB;
+        resultado[ 1 ] = scoreB;
+
+
+    } else {
+
+        a[ 2 ] = 0;
+        b[ 2 ] = 0;
+        
+    }
+    
+    return resultado;
+}
+
+function main() {
+
+
+    // Gabarito = [ 1, 1 ]
+    //
+
+    var tripletA = [ 6, 9, 7 ];
+    var tripletB = [ 4, 9, 10 ];
+    
+    printFunc = comparaTriplets( tripletA, tripletB );
+
+    console.log( printFunc );
+}
+
+main();
